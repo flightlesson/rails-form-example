365bf13eece12dd39e44f72eddc146db16123809
Timothy E Singletary <tsingle@muddypaddock.com>
2017-06-06 07:57:42 -0400

    add a little robustness to portals controller

--- userperms/app/controllers/portals_controller.rb
+++ userperms/app/controllers/portals_controller.rb
@@ -2,9 +2,14 @@ class PortalsController < ApplicationController
   def index
 
     Rails.logger.info("PortalsController#index: page is " + (params[:page] || 'index'))
-    
-    respond_to do |format|
-      format.html { render template: "portals/#{(params[:page] || 'index')}.html" }
+    begin
+      respond_to do |format|
+        format.html { render template: "portals/#{(params[:page] || 'index')}.html" }
+      end
+    rescue ActionView::MissingTemplate => e
+      redirect_to :root
+    rescue => e
+      Rails.logger.info("rescue: " + e.class.to_s + ": " + e.inspect)
     end
   end
 end
